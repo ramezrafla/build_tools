97d5bfb87d55717ab8925dc9a1c0c9419b0293fd
Ramez Rafla <ramez@zegenie.com>
2020-04-23 02:20:15 -0400

    Fixed generate_themes

--- custom/generate_themes.sh
+++ custom/generate_themes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=/home/ubuntu/build_tools/out/linux_64/onlyoffice/documentserver
-$DIR/server/tools/allthemesgen --converter-dir=$DIR/server/FileConverter/bin --src=$DIR/sdkjs/slide/themes --output=$DIR/sdkjs/common/Images
+LD_LIBRARY_PATH=$DIR/server/FileConverter/bin $DIR/server/tools/allthemesgen --converter-dir=$DIR/server/FileConverter/bin --src=$DIR/sdkjs/slide/themes --output=$DIR/sdkjs/common/Images
